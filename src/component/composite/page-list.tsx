adb09a5b7b6f43916854cd258a0d019e6f09d9e9
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-03-27 15:24:45 +0200

    chore(store): rebased from master
    ;

--- src/component/composite/page-list.tsx
+++ src/component/composite/page-list.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import Layout from '../../lsg/patterns/layout';
-import { PageRef } from '../../store/project/page-ref';
+import { PageRef } from '../../store/page/page-ref';
 import { PreviewTile } from '../../lsg/patterns/preview-tile';
 import Space, { Size } from '../../lsg/patterns/space';
 
@@ -11,21 +11,20 @@ export interface PageListProps {
 	editable: boolean;
 	focused: boolean;
 	handleChange: React.ChangeEventHandler<HTMLInputElement>;
-	onEdit: any;
 	pages: PageRef[];
 	value: string;
 	handleClick(e: React.MouseEvent<HTMLElement>): void;
+	onEdit(): void;
 }
 
 export const PageList: React.StatelessComponent<PageListProps> = (props): JSX.Element => (
 	<Layout>
 		{props.pages.map((page: PageRef, i: number) => {
-			let editable = false;
 			if (page.getId() === props.activePage) {
 				// console.log(props.onFocus());
 			}
 			if (page.getId() === props.activeTitle) {
-				editable = props.onEdit();
+				props.onEdit();
 			}
 
 			console.log(props, 'this are the props');
@@ -33,7 +32,7 @@ export const PageList: React.StatelessComponent<PageListProps> = (props): JSX.El
 				<Space key={i} size={Size.S}>
 					<PreviewTile
 						id={page.getId()}
-						editable={editable}
+						editable={false}
 						focused={props.focused}
 						handleChange={props.handleChange}
 						name={page.getName()}
